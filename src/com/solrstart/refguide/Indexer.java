cf88a4de29433f79cea54c3440efd49c807bca26
arafalov <arafalov@gmail.com>
2018-11-12 08:20:23 -0500

    Add basic indexing into Solr. Not yet searching well for class names.

--- src/com/solrstart/refguide/Indexer.java
+++ src/com/solrstart/refguide/Indexer.java
@@ -1,21 +1,31 @@
 package com.solrstart.refguide;
 
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrInputDocument;
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.ast.Document;
-import org.asciidoctor.ast.StructuralNode;
+import org.asciidoctor.ast.*;
 
 import static org.asciidoctor.OptionsBuilder.options;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
 public class Indexer {
 
     private static TreeSet<String> CONTEXTS = new TreeSet<>();
+    private static ConcurrentUpdateSolrClient solrClient;
+    private static String fileName;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SolrServerException {
         File testFile = new File(args[0]);
-        System.out.println("Testing with: " + testFile.getName());
+
+        fileName = testFile.getName();
+        System.out.println("Testing with: " + fileName);
 
         Asciidoctor asciidoctor = Asciidoctor.Factory.create();
         Map<String, Object> options = options().asMap();
@@ -24,12 +34,138 @@ public class Indexer {
 
         System.out.println(document.getDoctitle());
 
-        printStructure(document.getBlocks(), 0);
+        solrClient = new ConcurrentUpdateSolrClient.Builder("http://localhost:8983/solr/refguide").build();
+        solrClient.deleteByQuery("*:*");
+        solrClient.commit();
+
+        ArrayDeque<String> titles = new ArrayDeque<>();
+
+        indexStructure(document, titles);
+
+//        printStructure(document.getBlocks(), 0);
 
 
         System.out.println("\n\n\nCONTEXTS:");
         System.out.println(CONTEXTS);
+        solrClient.commit();
+        solrClient.close();
     }
+
+    private static void indexStructure(StructuralNode parentNode, ArrayDeque<String> titles) throws IOException, SolrServerException {
+        String anchor = null;
+        String context = parentNode.getContext();
+        switch (context) {
+            case "document":
+                Document doc = (Document) parentNode;
+                titles.addLast(doc.getDoctitle());
+                break;
+            case "preamble":
+                titles.addLast("Preamble");
+                break;
+            case "section":
+                Section section = (Section) parentNode;
+                titles.addLast(section.getTitle());
+                anchor = section.getId();
+                break;
+            default:
+                System.err.println("Unknown parent node: " + context);
+        }
+
+        System.out.println("Looking at: "  + String.join(" >> ", titles));
+
+        List<String> children = indexChildren(parentNode.getBlocks(), titles);
+
+        if (children != null) {
+            SolrInputDocument doc = new SolrInputDocument();
+            doc.addField("id", fileName + ':' + anchor);
+            if (anchor != null) { doc.addField("anchor", anchor); }
+            doc.addField("path", String.join(" >> ", titles));
+            doc.addField("title", titles.getLast()); //for better matching
+            doc.addField("level", parentNode.getLevel());
+            doc.addField("text", children);
+            UpdateResponse add = solrClient.add(doc);
+
+        }
+
+        titles.removeLast();
+
+
+//        System.out.printf("%s%d: %s (Style: %s, ContentModel: %s, Context: %s, Class: %s)\n",
+//                "", parentNode.getLevel(),
+//                parentNode.getId(), parentNode.getStyle(), parentNode.getContentModel(), parentNode.getContext(),
+//                parentNode.getClass().getSimpleName()
+//        );
+    }
+
+    private static List<String> indexChildren(List<StructuralNode> nodes, ArrayDeque<String> titles) throws IOException, SolrServerException {
+        if (nodes == null || nodes.isEmpty()) { return null; }
+
+        List<String> children = new ArrayList<>();
+
+        for (StructuralNode node : nodes) {
+            String context = node.getContext();
+            switch (context) {
+                case "section":
+                case "preamble":
+                    indexStructure(node, titles);
+                    break;
+                case "paragraph":
+                case "listing":
+                case "admonition":
+                    Block block = (Block) node;
+                    children.addAll(block.getLines());
+                    break;
+                case "dlist":
+                    DescriptionList dlist = (DescriptionList) node;
+                    if (dlist.hasItems()) {
+                        for (DescriptionListEntry entry : dlist.getItems()) {
+                            for (ListItem term : entry.getTerms()) {
+                                children.add(term.getSource());
+                            }
+                            children.add(entry.getDescription().getSource());
+                        }
+
+                    }
+                    break;
+                case "ulist":
+                    org.asciidoctor.ast.List list = (org.asciidoctor.ast.List) node;
+                    for (StructuralNode listItemNode : list.getItems()) {
+                        ListItem listItem = (ListItem) listItemNode;
+                        children.add(listItem.getSource());
+                    }
+                    break;
+
+                case "table":
+                    Table table = (Table) node;
+                    extractCells(table.getHeader(), children);
+                    extractCells(table.getBody(), children);
+                    extractCells(table.getFooter(), children);
+                    break;
+                default:
+                    System.err.println("UNKNOWN CHILD NODE: " + context);
+                    children.add(node.convert());
+            }
+        }
+//        //stub
+//        children.add("Content 1");
+//        children.add("Content 2");
+
+        // process various child types
+        // if child is a section, recurse
+
+        return children;
+    }
+
+    private static void extractCells(List<Row> rows, List<String> children) {
+        if (rows == null) { return; }
+
+        for (Row row: rows) {
+            for (Cell cell : row.getCells()) {
+                children.add(cell.getSource());
+            }
+        }
+    }
+
 
     private static String OFFSET = "                                                         ";
 
@@ -47,6 +183,13 @@ public class Indexer {
                     node.getId(), node.getStyle(), node.getContentModel(), node.getContext(),
                     node.getClass().getSimpleName()
                     );
+            if (node instanceof Block) {
+                List<String> lines = ((Block) node).getLines();
+                int linesCount = lines.size();
+                if (linesCount > 0) {
+                    System.out.printf("%s  (1 of %d): %s\n", currentOffset, linesCount, lines.get(0));
+                }
+            }
 //            System.out.printf("%svvvv\n%s\n%1$s^^^^\n", currentOffset, block.getContent());
             printStructure(node.getBlocks(), offsetLevel+1);
         }
